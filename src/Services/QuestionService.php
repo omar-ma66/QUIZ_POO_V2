5125230888bd70e64872e0e6caa0244ce2851e38
omar-ma66 <devcodefree66@gmail.com>
2026-05-12 09:12:47 +0200

    creation d'une class QuestionService

--- src/Services/QuestionService.php
+++ src/Services/QuestionService.php
@@ -3,10 +3,24 @@
 
 class QuestionService{
  private   QuestionRepository $obj  ; 
+ private   $data = [];
+ private   PDO $bdd ;
+ private    int $limit;
+ private    int $index;
 
-public function __construct(PDO $idcon , int $limit)
+public function __construct(PDO $idcon , int $limit,string $tableName)
 {
-   // $obj = new QuestionRepository($idcon, "questions") ;
+   $this->obj = new QuestionRepository($idcon, $tableName) ;
+   $this->limit = $limit ;
+   $this->bdd = $idcon ;
+   $this->index = 0 ;
+}
+
+public function generate():bool
+{
+  
+
+return true;
 }
 
 }
